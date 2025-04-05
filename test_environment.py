fd5ae759e23ddc7826797ea715e2f372f820d5a8
28250 <2825024660@qq.com>
2025-04-05 20:27:12 +0800

    环境测试

---
+++ test_environment.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+"""
+@author: wly
+@software: PyCharm
+@file: test_environment.py
+@time: 2025/4/5 20:21
+"""
+import torch
+
+# 检查CUDA是否可用
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+print("device_name: {}".format(torch.cuda.get_device_name(0)))
