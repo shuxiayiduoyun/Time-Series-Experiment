49be28385ce40b6d4eb3f766de0c61c8b70399df
28250 <2825024660@qq.com>
2025-04-07 10:07:32 +0800

    run脚本测试

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -6,18 +6,64 @@
  @File    : exp_long_term_forecasting.py.py
  @Description: 
 """
+import os
+import time
+import torch.nn as nn
+from torch import optim
+
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
+from utils.tools import EarlyStopping
 
 
 class Exp_Long_Term_Forecast(Exp_Basic):
     def __init__(self, args):
         super(Exp_Long_Term_Forecast, self).__init__(args)
 
-    def __build_model(self):
+    def _build_model(self):
         model = self.model_dict[self.args.model].Model(self.args).float()
         return model
 
-    def __get_data(self, flag):
+    def _get_data(self, flag):
         data_set, data_loader = data_provider(self.args, flag)
         return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        return model_optim
+
+    def _select_criterion(self):
+        criterion = nn.MSELoss()
+        return criterion
+
+    def train(self, setting):
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
+
+        path = os.path.join(self.args.checkpoints, setting)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        time_now = time.time()
+
+        train_steps = len(train_loader)
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
+        for epoch in range(self.args.train_epochs):
+            iter_count = 0
+            train_loss = []
+
+            self.model.train()
+            epoch_time = time.time()
+
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+                iter_count += 1
+                model_optim.zero_grad()
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
--- 一些操作/run测试.py
+++ 一些操作/run测试.py
@@ -33,12 +33,36 @@ if __name__ == '__main__':
     parser.add_argument('--features', type=str, default='M')
     parser.add_argument('--target', type=str, default='OT')
     parser.add_argument('--freq', type=str, default='h')
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints/')
     # forecasting task
     parser.add_argument('--seq_len', type=int, default=96)
     parser.add_argument('--label_len', type=int, default=48)
     parser.add_argument('--pred_len', type=int, default=96)
+    parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
+    # model define
+    parser.add_argument('--enc_in', type=int, default=7)
+    parser.add_argument('--dec_in', type=int, default=7)
+    parser.add_argument('--c_out', type=int, default=7)
+    parser.add_argument('--d_model', type=int, default=512)
+    parser.add_argument('--n_heads', type=int, default=8)
+    parser.add_argument('--e_layers', type=int, default=2)
+    parser.add_argument('--d_layers', type=int, default=1)
+    parser.add_argument('--d_ff', type=int, default=2048)
+    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--factor', type=int, default=3)
+    parser.add_argument('--embed', type=str, default='timeF')
+    parser.add_argument('--output_attention', action='store_true', default=True)
+    parser.add_argument('--activation', type=str, default='gelu')
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=1)
+    parser.add_argument('--train_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--patience', type=int, default=3)
+    parser.add_argument('--learning_rate', type=float, default=0.0001)
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True)
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--use_multi_gpu', action='store_true', default=False)
     args = parser.parse_args()
     args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
 
@@ -47,3 +71,8 @@ if __name__ == '__main__':
 
     if args.task_name == 'long_term_forecast':
         Exp = Exp_Long_Term_Forecast
+
+    if args.is_training:
+        exp = Exp(args)
+        print('>>>>>>>start training : >>>>>>>>>>>>>>>>>>>>>>>>>>')
+        exp.train('save_models')
