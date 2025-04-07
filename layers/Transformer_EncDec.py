483080be7e5e616fd7dfecd67ab287c367ceb0bd
28250 <2825024660@qq.com>
2025-04-07 17:04:21 +0800

    Transformer调试

--- layers/Transformer_EncDec.py
+++ layers/Transformer_EncDec.py
@@ -6,11 +6,7 @@ import torch.nn.functional as F
 class ConvLayer(nn.Module):
     def __init__(self, c_in):
         super(ConvLayer, self).__init__()
-        self.downConv = nn.Conv1d(in_channels=c_in,
-                                  out_channels=c_in,
-                                  kernel_size=3,
-                                  padding=2,
-                                  padding_mode='circular')
+        self.downConv = nn.Conv1d(in_channels=c_in, out_channels=c_in, kernel_size=3, padding=2, padding_mode='circular')
         self.norm = nn.BatchNorm1d(c_in)
         self.activation = nn.ELU()
         self.maxPool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
