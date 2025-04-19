0a013019ec76fc4504750aa741adcae5e91c091b
28250 <2825024660@qq.com>
2025-04-19 20:03:07 +0800

    环境测试

--- test_environment.py
+++ test_environment.py
@@ -6,8 +6,11 @@
 @time: 2025/4/5 20:21
 """
 import torch
+import sys
 
 # 检查CUDA是否可用
 print("CUDA available:", torch.cuda.is_available())
 print("CUDA version:", torch.version.cuda)
 print("device_name: {}".format(torch.cuda.get_device_name(0)))
+# python版本
+print("python version: {}".format(sys.version))
